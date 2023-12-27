6ef0dddac3464a65c0a6903b13e089c0dd4554d1
Shandea <86807815+Shandea@users.noreply.github.com>
2023-12-27 10:54:27 -0800

    Initial Setup

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+const cors = require("cors");
+
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.use(express.json());
+
+// Routes
+app.use(require());
+
+// Get MongoDB driver connection
+const dbo = require("./db/conn");
+ 
+app.listen(port, () => {
+  // Perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
+});
