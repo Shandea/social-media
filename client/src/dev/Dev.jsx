04b9d6135cabf68ca43f8258f1ceca5f097d8d4f
mrmcgain <michael.r.mcgrain@gmail.com>
2023-12-28 12:14:00 -0700

    more inputs

--- client/src/dev/Dev.jsx
+++ client/src/dev/Dev.jsx
@@ -5,7 +5,21 @@ import axios from 'axios'
 
 function Dev() {
 
-    const [reg, setReg] = useState({})
+    const [reg, setReg] = useState({
+        username: "",
+        password: "",
+        confirmPassword: "",
+        location: {
+            city: "",
+            state: "",
+            zipcode: ""
+        },
+        secretQuestion: "",
+        secretAnswer: "",
+        age: "",
+        gender: "",
+
+    })
 
     const handleChange = (e) => {
         setReg(prev => ({
@@ -37,13 +51,38 @@ function Dev() {
                 onChange={(e) => handleChange(e)}
             >
             </input>
-            
+
             <input name="password"
                 placeholder='password'
                 value={reg.password || ""}
                 onChange={(e) => handleChange(e)}
             >
             </input>
+
+            <input
+                    type="text"
+                    placeholder="State"
+                    name="state"
+                    required
+                    value={reg.location.state || ""}
+                    onChange={(e) => setReg(prev => ({ ...prev, location: { ...prev.location, [e.target.name]: e.target.value } }))} 
+                    />
+            <input
+                    type="text"
+                    placeholder="City"
+                    name="city"
+                    required
+                    value={reg.location.city || ""}
+                    onChange={(e) => setReg(prev => ({ ...prev, location: { ...prev.location, [e.target.name]: e.target.value } }))} 
+                    />
+            <input
+                    type="text"
+                    placeholder="ZipCode"
+                    name="zipcode"
+                    required
+                    value={reg.location.zipcode || ""}
+                    onChange={(e) => setReg(prev => ({ ...prev, location: { ...prev.location, [e.target.name]: e.target.value } }))} 
+                    />
 
             <button onClick={(e) => handleSubmit(e)}>submit</button>
 
