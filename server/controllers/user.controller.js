d466c0b3d7b669dc4f8b78a99ca05ba73f774f5c
mrmcgain <michael.r.mcgrain@gmail.com>
2023-12-28 10:43:12 -0700

    fixed promises v.2

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -12,20 +12,21 @@ module.exports = {
 
     register: (req, res) => {
         console.log("register attempt", req.body)
-        User.find({ username: req.body.username })
+
+        User.findOne({ username: req.body.username })
             .then(found => {
+                console.log("found", found)
+                // if (!req.body.password) {
+                //     res.json("Register error, please fill out fields")
+                // }
 
-                if(!req.body.password){
-                    res.json("Register error, please fill out fields")
-                }
-
-                if (found.length) {
+                if (found) {
                     res.json({ message: "Registration Error" })
 
-                } else {
+                } else  {
 
                     const hash = bcrypt.hashSync(req.body.password, 10)
-                    const newUSer = new User({
+                    const newUser = new User({
                         username: req.body.username,
                         email: req.body.email,
                         password: hash,
@@ -40,22 +41,22 @@ module.exports = {
                         birthdate: req.body.birthday
 
                     })
+                    User.create(newUser)
+                        .then(created => {
+                            console.log("created User", created)
+                            res.json({ message: "Success", user: created })
+
+                            // on successful reg, give a jwt and auth to site
+                        })
                 }
             })
-            .then(() => {
-
-
-                User.create(newUser)
-                    .then(created => {
-                        console.log("created User", created)
-                        res.json({ message: "Success", user: created })
-
-                        // on successful reg, give a jwt and auth to site
-                    })
-
-
-            })
             .catch(err => console.log("err", err))
+            // .then(() => {
+            // console.log("new user attempt", newUser)
+
+
+
+            // })
     },
 
 
@@ -64,7 +65,7 @@ module.exports = {
 
 
 }
-    
+
 
 
 
