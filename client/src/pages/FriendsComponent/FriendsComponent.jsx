7fe3b3d98795a331a108e106bf68cd72653e8c93
GrahamDykes <153469002+GrahamDykes@users.noreply.github.com>
2023-12-29 09:40:24 -0700

    Added wireframe of feed components

---
+++ client/src/pages/FriendsComponent/FriendsComponent.jsx
@@ -0,0 +1,14 @@
+import './FriendsComponent.css'
+import React, { useState } from "react";
+
+const FriendsComponent = () => {
+  return (
+    <div className='friendscomponentcontainer'>
+        <h2>Friends</h2>
+        <p>Im a search box? maybe</p>
+        <p>feeeed me</p>
+        </div>
+  )
+}
+
+export default FriendsComponent
