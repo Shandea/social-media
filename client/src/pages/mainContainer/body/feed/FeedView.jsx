81c3d523a77fa7c9b91b9dd0416b4549815840a4
mrmcgain <michael.r.mcgrain@gmail.com>
2024-01-04 16:38:56 -0700

    images on feed are good

--- client/src/pages/mainContainer/body/feed/FeedView.jsx
+++ client/src/pages/mainContainer/body/feed/FeedView.jsx
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import FeedContainer from "../../../../components/feeds/FeedContainer"
 
-import AddFeed from "./AddFeed"
+import AddFeed from "../../../../components/feeds/AddFeed"
+
+import axios from 'axios'
 
 
 export default function Feed() {
     // feed container... a feed 
 
+
+    useEffect(() => {
+
+        axios({
+            method: "GET",
+            url: "http://localhost:5000/api/getfeeds",
+            withCredentials: true,
+        })
+            .then(res => {
+                console.log("res", res)
+                setFeeds(res.data)
+            })
+            .catch(err => console.log("get feed err", err))
+
+    }, [])
+
+    const [feeds, setFeeds] = useState([])
+
+
+
+
     return (
 
         <>
+            {console.log("feed state", feeds)}
             <div id="FeedTopAction"
                 style={{ border: "black 2px solid", display: 'flex', justifyContent: 'space-evenly' }}
             >
@@ -39,30 +63,27 @@ export default function Feed() {
             <div id="FeedAddActions">(add) - opens modal to add feed ,|| search area to search feeds in DB </div>
 
 
+            <AddFeed />
 
 
 
             {/* FOR ILLUSTRATIVE PURPOSE, this will be a map from the api endpoint rendering into <FeedContainer /> */}
 
             <div className='feedContainer'>
-                <FeedContainer />
+                {feeds.map((obj, i) => {
+                    return (
+                        <div key={i}>
+                            <FeedContainer obj={obj} />
+                        </div>
+                    )
+
+                })}
             </div>
 
-            <div className='feedContainer'>
-                <FeedContainer />
-            </div>
 
-            <div className='feedContainer'>
-                <FeedContainer />
-            </div>
-
-            <div className='feedContainer'>
-                <FeedContainer />
-            </div>
 
 
             {/* ???????????????????????/ */}
-            <AddFeed />
 
         </>
     )
