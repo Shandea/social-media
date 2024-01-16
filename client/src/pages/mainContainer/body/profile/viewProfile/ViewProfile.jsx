d365871d31ecf709b06bfed0bcdebaee9b574dc0
Brian Williams <152632135+bkwilliamsmerncode@users.noreply.github.com>
2024-01-16 15:12:34 -0700

    Add message button and add friend button to view profile page

--- client/src/pages/mainContainer/body/profile/viewProfile/ViewProfile.jsx
+++ client/src/pages/mainContainer/body/profile/viewProfile/ViewProfile.jsx
@@ -89,6 +89,15 @@ const ViewProfile = (props) => {
             {profileView.details?.maritalStatus || "Single"}
              </p>
             </div>
+            <hr className="line"/>
+            <div className="bottombtncontainer">
+              <div className="leftmessagebtn">
+                <button className="mabtn">Message</button>
+              </div>
+              <div className="rightaddbtn">
+                <button className="mabtn">Add Friend</button>
+              </div>
+            </div>
           </div>
 
           
