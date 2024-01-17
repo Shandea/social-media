c76b5b057e463e642eff5d3e2dd4912909c28d23
Corey Tarpley <developertarpley@gmail.com>
2024-01-17 13:46:32 -0700

    search friends functionality

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -304,7 +304,7 @@ module.exports = {
                             }
                         })
                 } else {
-                    // console.log("you can't add yourself")
+                     console.log("you can't add yourself")
                 }
             })
 
