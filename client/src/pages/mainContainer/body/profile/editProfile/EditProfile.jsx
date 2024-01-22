901f845bce277cad7f2d9d63e35b1fdbecd238e4
Corey Tarpley <developertarpley@gmail.com>
2024-01-22 12:59:21 -0700

    edit update correction

--- client/src/pages/mainContainer/body/profile/editProfile/EditProfile.jsx
+++ client/src/pages/mainContainer/body/profile/editProfile/EditProfile.jsx
@@ -47,37 +47,37 @@ const handleProfileUpdate = (e)=>{
       <label htmlFor="firstname" className='editprofilelabels'>First Name</label>
       <Inputs className="editprofileinputs"
       name={"firstname"}
-      defaultValue={userPro.firstName}
+      defaultValue={userPro.firstName || ""}
       />
       <label htmlFor="lastname" className='editprofilelabels'>Last Name</label>
       <Inputs className="editprofileinputs"
       name="lastname"
-      defaultValue={userPro.lastName}
+      defaultValue={userPro.lastName || ""}
       />
       <label htmlFor="email" className='editprofilelabels'>Email</label>
       <Inputs className="editprofileinputs"
       name="email"
-      defaultValue={userPro.email}
+      defaultValue={userPro.email || ""}
       />
       <label htmlFor="phone" className='editprofilelabels'>Phone Number</label>
       <Inputs className="editprofileinputs"
       name="phone"
-      defaultValue={userPro.phone}
+      defaultValue={userPro.phone || ""}
       />
       <label htmlFor="location" className='editprofilelabels'>City</label>
       <Inputs className="editprofileinputs"
       name="location.city"
-      defaultValue={userPro.location.city}
+      defaultValue={userPro.location.city || ""}
       />
       <label htmlFor="location" className='editprofilelabels'>State</label>
       <Inputs className="editprofileinputs"
       name="location.state"
-      defaultValue={userPro.location.state}
+      defaultValue={userPro.location.state || ""}
       />
       <label htmlFor="location" className='editprofilelabels'>Zip Code</label>
       <Inputs className="editprofileinputs"
       name="location.zipcode"
-      defaultValue={userPro.location.zipcode}
+      defaultValue={userPro.location.zipcode || ""}
       />
       <label htmlFor="pronoun" className='editprofilelabels'>Pronoun</label>
       <Inputs className="editprofileinputs"
@@ -92,7 +92,7 @@ const handleProfileUpdate = (e)=>{
       <label htmlFor="gender" className='editprofilelabels'>Gender</label>
       <Inputs className="editprofileinputs"
       name="gender"
-      defaultValue={userPro.gender}
+      defaultValue={userPro.gender || ""}
       />
       {/* <Inputs
       name=""
